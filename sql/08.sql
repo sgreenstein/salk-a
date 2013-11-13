8906acf23b47b636ca30c124634c10c21b69af52
Zach Hall <zach@sosguy.net>
2013-11-13 16:13:31 -0500

    User table data.

---
+++ sql/08.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users` (`id`, `username`, `password`, `level`, `profile_picture`, `first_name`, `last_name`, `created`, `modified`, `site_id`, `camp_id`) VALUES
+(1, 'admin', '4824dfa5b9ad27a24c4465eeeb60c5157a469fa6', 100, NULL, 'The', 'Admin', '2013-11-06 14:13:03', '2013-11-06 15:50:10', NULL, NULL),
+(2, 'camper', '725ff77885215dfd5c9bfef4d0c2aa0ea03214df', 20, NULL, NULL, NULL, '2013-11-06 16:07:30', '2013-11-06 16:07:30', NULL, NULL),
+(4, 'testCamper001', '725ff77885215dfd5c9bfef4d0c2aa0ea03214df', NULL, NULL, NULL, NULL, '2013-11-10 15:38:53', '2013-11-10 15:38:53', NULL, NULL),
+(5, 'testCamper002', '725ff77885215dfd5c9bfef4d0c2aa0ea03214df', 20, NULL, NULL, NULL, '2013-11-10 15:41:04', '2013-11-10 15:41:04', NULL, NULL),
+(6, 'testParent001', '725ff77885215dfd5c9bfef4d0c2aa0ea03214df', 10, NULL, NULL, NULL, '2013-11-10 15:47:31', '2013-11-10 15:47:31', NULL, NULL);
