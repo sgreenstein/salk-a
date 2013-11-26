07946952a16f7a9a63529b4da7dc43e196b85ac2
CSATeam <csateam@salk6-sam.(none)>
2013-11-25 20:32:20 -0500

    "Added CakeStrap and made the default look better"

---
+++ app/Controller/Controller/CampsController.php
@@ -0,0 +1,113 @@
+<?php
+class CampsController extends AppController {
+
+	public function beforeFilter() {
+		parent::beforeFilter();
+	}
+	//views all camps
+	public function index() {
+		$this->set('camps', $this->Camp->find('all'));
+	}
+	//views one camp
+	public function view($id = null) {
+		if(!$id) {
+			throw new NotFoundException(__('Invalid camp'));
+		}
+
+		$camp = $this->Camp->findById($id);
+		if(!$camp) {
+			throw new NotFoundException(__('Invalid camp'));
+		}
+		$this->set('camp', $camp);
+	}
+	//creates a new camp
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Camp->create();
+			if ($this->Camp->save($this->request->data)) {
+				$this->Session->setFlash(__('Camp successfully created.'));
+				return $this->redirect(array('action' => 'index'));
+			}
+			$this->Session->setFlash(__('Camp could not be created.'));
+		}
+	}
+	//edits an existing camp
+	public function edit($id = null) {
+		if (!$id) {
+			throw new NotFoundException(__('Invalid camp'));
+		}
+		$camp = $this->Camp->findById($id);
+		if (!$camp) {
+			throw new NotFoundException(__('Invalid camp'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			$this->Camp->id = $id;
+			if ($this->Camp->save($this->request->data)) {
+				$this->Session->setFlash(__('The camp has been updated.'));
+				return $this->redirect(array('action' => 'index'));
+			}
+			$this->Session->setFlash(__('The camp could not be updated. Please try again.'));
+		}
+		if (!$this->request->data) {
+			$this->request->data = $camp;
+		}
+	}
+	//deletes a camp
+	public function delete($id = null) {
+		if(!$id) {
+			throw new NotFoundException(__('Invalid camp'));
+		}
+		$camp = $this->Camp->findById($id);
+		if(!$camp) {
+			throw new NotFoundException(__('Invalid camp'));
+		}		
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if($this->Camp->delete($id, true)) {
+				$this->Session->setFlash(__('Camp deleted'));
+				return $this->redirect(array('action' => 'index'));
+			}
+		}
+		$this->Session->setFlash(__('Could not delete the camp'));
+	}
+	//adds a site to this camp
+	public function addSite($id = null) {
+		if(!$id) {
+			throw new NotFoundException(__('Invalid camp'));
+		}
+		$camp = $this->Camp->findById($id);
+		if(!$camp) {
+			throw new NotFoundException(__('Invalid camp'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			$this->Camp->Site->create();
+			$this->request->data['Site']['camp_id'] = $id;
+			if($this->Camp->Site->save($this->request->data)) {
+				$this->Session->setFlash(__('Site added'));
+				return $this->redirect(array('action' => 'view', $id));
+			}
+		}
+		else {
+			$this->Session->setFlash(__('Site could not be added.'));
+		}
+	}
+	public function isAuthorized($user) {
+		if (!isset($user)) return false;
+		switch($this->action) {
+			// camp director can access own camp
+			case 'edit':
+			case 'addSite':
+				if($user['camp_id'] == $this->request->params['pass']['0'])
+					return true;
+				break;
+			case 'deleteSite':
+				$camp = $this->Camp->findById($this->request->params['pass']['0']);
+				if($user['camp_id'] == $camp['Camp']['id'])
+					return true;
+			// campers, parents can view their camp
+			case 'view':
+				if($this->Camp->isUserInCamp($user['id'], $this->request->params['pass']['0']))			
+					return true;
+		}
+		return parent::isAuthorized($user);
+	}
+}
--- app/View/Layouts/default.ctp
+++ app/View/Layouts/default.ctp
@@ -4,59 +4,73 @@
  * PHP 5
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.View.Layouts
+ * @package       Cake.View.Layouts
  * @since         CakePHP(tm) v 0.10.0.1076
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
 $cakeDescription = __d('cake_dev', 'CakePHP: the rapid development php framework');
 ?>
-<!DOCTYPE html>
+<?php echo $this->Html->docType('html5'); ?> 
 <html>
-<head>
-	<?php echo $this->Html->charset(); ?>
-	<title>
-		<?php echo $cakeDescription ?>:
-		<?php echo $title_for_layout; ?>
-	</title>
-	<?php
-		echo $this->Html->meta('icon');
+	<head>
+		<?php echo $this->Html->charset(); ?>
+		<title>
+			<?php echo $cakeDescription ?>:
+			<?php echo $title_for_layout; ?>
+		</title>
+		<?php
+			echo $this->Html->meta('icon');
+			
+			echo $this->fetch('meta');
 
-		echo $this->Html->css('cake.generic');
+			echo $this->Html->css('bootstrap');
+			echo $this->Html->css('main');
+			echo $this->Html->css('bootstrap_adjustments');
 
-		echo $this->fetch('meta');
-		echo $this->fetch('css');
-		echo $this->fetch('script');
-	?>
-</head>
-<body>
-	<div id="container">
-		<div id="header">
-			<h1><?php echo $this->Html->link($cakeDescription, 'http://cakephp.org'); ?></h1>
-		</div>
-		<div id="content">
+			echo $this->fetch('css');
+			
+			echo $this->Html->script('libs/jquery-1.10.2.min');
+			echo $this->Html->script('libs/bootstrap.min');
+			
+			echo $this->fetch('script');
+		?>
+	</head>
 
-			<?php echo $this->Session->flash(); ?>
+	<body>
 
-			<?php echo $this->fetch('content'); ?>
-		</div>
-		<div id="footer">
-			<?php echo $this->Html->link(
-					$this->Html->image('cake.power.gif', array('alt' => $cakeDescription, 'border' => '0')),
-					'http://www.cakephp.org/',
-					array('target' => '_blank', 'escape' => false)
-				);
-			?>
-		</div>
-	</div>
-	<?php //echo $this->element('sql_dump'); ?>
-</body>
+		<div id="main-container">
+		
+			<div id="header" class="container">
+				<?php echo $this->element('menu/top_menu'); ?>
+			</div><!-- /#header .container -->
+			
+			<div id="content" class="container">
+				<?php echo $this->Session->flash(); ?>
+				<?php echo $this->fetch('content'); ?>
+			</div><!-- /#content .container -->
+			
+			<div id="footer" class="container">
+				<?php //Silence is golden ?>
+			</div><!-- /#footer .container -->
+			
+		</div><!-- /#main-container -->
+		
+		<div class="container">
+			<div class="well well-sm">
+				<small>
+					<?php echo $this->element('sql_dump'); ?>
+				</small>
+			</div><!-- /.well well-sm -->
+		</div><!-- /.container -->
+		
+	</body>
+
 </html>
